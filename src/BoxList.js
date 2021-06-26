ed68ab12292ef865565566b94d64db3d78bc2b0b
vineeth123-png <vineethdvs123@gmail.com>
2021-06-26 09:50:22 +0530

    added NewBoxForm component that adds new boxes to the boxlist component via forms

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
 
 class BoxList extends Component {
 	constructor(props) {
@@ -7,12 +8,23 @@ class BoxList extends Component {
 		this.state = {
 			boxes: [{ width: 10, height: 20, color: "orange" }]
 		};
+		this.create = this.create.bind(this);
+	}
+	create(newBox) {
+		this.setState({
+			boxes: [...this.state.boxes, newBox]
+		});
 	}
 	render() {
 		const boxes = this.state.boxes.map((box, idx) => (
 			<Box height={box.height} width={box.width} color={box.color} />
 		));
-		return <div>{boxes}</div>;
+		return (
+			<div>
+				<NewBoxForm createBox={this.create} />
+				{boxes}
+			</div>
+		);
 	}
 }
 
